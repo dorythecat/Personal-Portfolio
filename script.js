32b11a955d0e2264b2e8a03e8a7903aa045e3228
Dory <pemegomachado@gmail.com>
2026-01-22 15:59:25 +0100

    Optimized nextProjects() function

--- script.js
+++ script.js
@@ -58,14 +58,16 @@ let projectIndex = 0;
 function nextProjects() {
     if (projects.length < 4) return;
     projectIndex++;
-    if (projectIndex > Math.floor(projects.length / 3)) projectIndex = 0;
+    const lengthMod = Math.floor(projects.length / 3);
+    if (projectIndex > lengthMod) projectIndex = 0;
     if (projectIndex > 0) {
+        const j = 3 * projectIndex;
         for (let i = 0; i < 3; i++) {
-            projects[3 * projectIndex - i - 1].classList.add("hidden");
-            projects[3 * projectIndex + i]?.classList.remove("hidden");
+            projects[j - i - 1].classList.add("hidden");
+            projects[j + i]?.classList.remove("hidden");
         }
     } else {
-        const j = 3 * Math.ceil(projects.length / 3) - 1;
+        const j = 3 * lengthMod + 2;
         for (let i = 0; i < 3; i++) {
             projects[j - i]?.classList.add("hidden");
             projects[i].classList.remove("hidden");
