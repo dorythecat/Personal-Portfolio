c8fd553768c1e8b8b8db1e2c9eb67a4c8b0d60cb
Dory <pemegomachado@gmail.com>
2026-01-22 15:37:40 +0100

    Fixed a few edge cases with carrousel

--- script.js
+++ script.js
@@ -10,8 +10,9 @@ function nextProjects() {
             projects[3 * projectIndex + i]?.classList.remove("hidden");
         }
     } else {
+        const j = 3 * Math.ceil(projects.length / 3) - 1;
         for (let i = 0; i < 3; i++) {
-            projects[projects.length - i - 1]?.classList.add("hidden");
+            projects[j - i]?.classList.add("hidden");
             projects[i].classList.remove("hidden");
         }
     } setTimeout(nextProjects, 10000);
