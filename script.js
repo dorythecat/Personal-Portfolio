98d4c5353aa1bb0a8e565bdbf3f93dbbdf474f4c
Dory <pemegomachado@gmail.com>
2026-01-22 15:38:16 +0100

    Added auto-fetching of 'top' projects from GitHub

--- script.js
+++ script.js
@@ -1,7 +1,62 @@
 const projects = document.getElementById("projects").children;
 
+// Cookie utilities
+function getCookie(name) {
+    const parts = `; ${document.cookie}`.split(`; ${name}=`);
+    if (parts.length === 2) return parts.pop().split(';').shift();
+    return "";
+}
+
+function setCookie(name, value, expiry = null) {
+    let expires = "";
+    if (expiry) {
+        let date = new Date();
+        date.setTime(date.getTime() + expiry);
+        expires = "; expires=" + date.toUTCString();
+    } document.cookie = name + "=" + (value || "")  + expires + "; path=/";
+}
+
+function delCookie(name) { document.cookie = name +'=; Path=/; Expires=Thu, 01 Jan 1970 00:00:01 GMT;'; }
+
+// GitHub API
+const API_URL = "https://api.github.com/user/45130233"
+
+// Get basic user info
+const request = new XMLHttpRequest();
+request.onreadystatechange = function() {
+    if (request.readyState !== 4 || request.status !== 200) return;
+    const response = JSON.parse(request.responseText);
+    setCookie("name", response["login"], 3600);
+    setCookie("html_url", response["html_url"], 3600);
+    setCookie("avatar_url", response["avatar_url"], 3600);
+
+    // Select repositories to showcase
+    const repoRequest = new XMLHttpRequest();
+    repoRequest.onreadystatechange = function() {
+        if (repoRequest.readyState !== 4 || repoRequest.status !== 200) return;
+        const response = JSON.parse(repoRequest.responseText);
+        let j = 0;
+        for (let i = 0; i < response.length; i++) {
+            const repo = response[i];
+            const score = Number(repo["stargazers_count"]) + Number(repo["watchers_count"]);
+            if (score < 1 || repo["fork"]) continue;
+            document.getElementById("projects").innerHTML += `
+                 <div class="${j++ > 2 ? "hidden" : ""}">
+                     <h3><a href="${repo["html_url"]}">${repo["name"]}</a></h3>
+                     <p>${repo["description"] || ""}</p>
+                 </div>
+            `;
+        }
+    }
+    repoRequest.open("GET", response["repos_url"], true);
+    repoRequest.send(null);
+}
+request.open("GET", API_URL, true);
+request.send(null);
+
 let projectIndex = 0;
 function nextProjects() {
+    if (projects.length < 4) return;
     projectIndex++;
     if (projectIndex > Math.floor(projects.length / 3)) projectIndex = 0;
     if (projectIndex > 0) {
@@ -16,5 +71,4 @@ function nextProjects() {
             projects[i].classList.remove("hidden");
         }
     } setTimeout(nextProjects, 10000);
-}
-setTimeout(nextProjects, 10000);
+} setTimeout(nextProjects, 10000);
